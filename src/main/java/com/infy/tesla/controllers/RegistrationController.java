53745de8ede931672b768843e047b37604cbb673
Saptarshi Chatterjee <schatterjee@hawk.iit.edu>
2019-10-05 12:52:44 -0500

    controller works

--- src/main/java/com/infy/tesla/controllers/RegistrationController.java
+++ src/main/java/com/infy/tesla/controllers/RegistrationController.java
@@ -1,12 +1,21 @@
 package com.infy.tesla.controllers;
 import model.User;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/ktts")
 public class RegistrationController {
     @PostMapping("/register")
     public String saveUser(@ModelAttribute User user) {
-        return user.getFname();
+        // TODO Auto-generated method stub
+        return "Kubra";
     }
 }
